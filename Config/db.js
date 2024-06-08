51a3c12261058d7bb2a3c6de0a48aa1b8edc5dae
shivavigneshwaran <shivvignesh98112@gmail.com>
2024-06-08 11:22:46 +0530

    configchanges

--- Config/db.js
+++ Config/db.js
@@ -1,14 +1,16 @@
-
 const mongoose = require('mongoose');
-getDataBase().catch(err => console.log(err));
+require('dotenv').config();
 
-async function getDataBase(){
+async function getDataBase() {
     //DataBase Connection With MongoDB
-const encodedPassword = encodeURIComponent("098112@kec");
-await mongoose.connect(`mongodb+srv://sivvignesh:${encodedPassword}@cluster0.o131g3b.mongodb.net/`).then(()=>{
-    console.log('Database connected...!');
-});
-   
+    const encodedPassword = encodeURIComponent(process.env.DB_PASSWORD);
+    const dbUrl = `mongodb+srv://sivvignesh:${encodedPassword}@cluster0.o131g3b.mongodb.net/`;
+    await mongoose.connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('Database connected...!');
+    }).catch(err => console.log('Database connection error:', err));
 }
 
 module.exports = {
