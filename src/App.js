18ccdbe013b1cf257aa16f6cd01223f36502e51f
Siva <Shivavigneshwaran>
2024-06-08 14:47:30 +0530

    pull

--- src/App.js
+++ src/App.js
@@ -30,9 +30,6 @@ function App() {
     const error = useSelector((state) => state?.products?.error);
     const productCount = useSelector((state) => state?.productCount?.count);
 
-
-
-
   useEffect(()=>{
     dispatch(fetchProduct());
     dispatch(addCount(totalQuantity));
