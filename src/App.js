30a216e2599281d3c7f2805e28abbb140a620ac8
shivavigneshwaran <163272654+shivavigneshwaran@users.noreply.github.com>
2024-06-09 20:24:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -60,6 +60,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Shop />} />
             <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+            <Route path='https://shopper-004m.onrender.com/mens' element={<ShopCategory banner={men_banner} category="men" />} />
             <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
             <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid" />} />
             <Route path='/product'>
