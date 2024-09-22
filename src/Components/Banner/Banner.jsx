aa2c96b59993527cd0362a2025e42f427ee1b0ad
shivavigneshwaran <163272654+shivavigneshwaran@users.noreply.github.com>
2024-09-22 19:53:15 +0530

    Update Banner.jsx

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -96,9 +96,9 @@ const Banner=()=>{
             <SimpleGrid {...template} >
                 <Flex {...templatebox}> 
                 <Stack {...bannerleft} padding={{base:"80px 80px",md:"50px 0px",lg:"80px 0px",xl:"80px 0px"}}>  
-                    <Heading {...bannerhead} fontSize="24px">Womens Collection</Heading>
+                    <Heading {...bannerhead} fontSize="30px">New Arraivals</Heading>
                     <HStack><Text fontSize="25px" fontWeight="300 !important">New</Text> <img src={hand_icon} style={{width:"60px"}} className="hero-hand-icon" alt="handimg" /></HStack>
-                    <Text cs="p" fontSize="25px" fontWeight="300 !important">Latest Collection for Womens</Text>
+                    <Text cs="p" fontSize="25px" fontWeight="300 !important">Collection for Womens</Text>
                     <Button {...sliderbtn}>Visit Collection <img src={arrow_icon} alt="Bannerbtnimg" /></Button>
                 </Stack>
                 </Flex>
