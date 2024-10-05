ffddc68ce8f0e6e516beb6c91c193228c7737a2e
shivavigneshwaran <shivvignesh98112@gmail.com>
2024-10-05 20:49:44 +0530

    added:address-bug fixed

--- routers/Address.js
+++ routers/Address.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {addAddress} = require('../Controllers/Address/Address');
+const {addAddress} = require('../Controllers/Address/address');
 
 router.post('/add-address',addAddress);
 module.exports = router;
