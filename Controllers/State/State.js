0be4c131dcd10368257d6c84a1af47b92a994a1a
shivavigneshwaran <shivvignesh98112@gmail.com>
2024-10-12 07:04:18 +0530

    added:states-getState

--- Controllers/State/State.js
+++ Controllers/State/State.js
@@ -24,6 +24,23 @@ const addState = async (req,res) => {
 		return res.status(500).json({status:"Fail",error:error});
 	}
 }
+
+const getStates = async (req,res,next)=>{
+	try{
+		const state = await State.find({deletedAt:null},"_id name code");
+		if(country){
+			return res.status(200).json({status:"Success",data:state})
+		}else{
+			return res.status(204).json({status:"Success",data:"State is Empty..!"});
+		}
+
+	}catch(error){
+		console.error('Error while saving Country:', error);
+        return res.status(500).json({ success: false, error:error,message:"Something Went Wrong..!"});
+
+	}
+}
 module.exports = {
-		addState
+		addState,
+		getStates
 }
--- routers/State.js
+++ routers/State.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
-const {addState} = require("../Controllers/State/State");
+const {addState,getStates} = require("../Controllers/State/State");
 
 router.post("/add-state",addState);
+router.post("/get-state",getStates);
 
 module.exports=router;
