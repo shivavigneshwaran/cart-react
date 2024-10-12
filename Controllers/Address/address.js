4fde0de4b14e8e25efcb8154211b17dec5781498
shivavigneshwaran <shivvignesh98112@gmail.com>
2024-10-12 13:51:40 +0530

    added:get-user-address based on user id in address tabe

--- Controllers/Address/address.js
+++ Controllers/Address/address.js
@@ -20,6 +20,31 @@ const addAddress = async (req,res)=>{
 	}
 }
 
+
+const getAddress = async (req,res)=>{
+
+	try{
+		const { id } = req.query;
+		if(!id){
+            return res.status(400).json({ message: 'User Id feild Required..!' });
+		}
+
+		const address = await Address.find({userId:id},"_id address pincode landmark cityTown")
+         .populate('stateId','name')
+         .populate('countryId','name')
+         .exec();
+         if(!address){
+			return res.status(204).json({status:"Success",data:"Address is Empty..!"});
+         }
+		return res.status(200).json({status:"success",data:address});
+	}catch(error){
+		console.log("Error: ", error);
+		return res.status(500).json({_status:"Fail",data:error.message || error});
+
+	}
+}
+
 module.exports = {
-	addAddress
+	addAddress,
+	getAddress
 }
--- routers/Address.js
+++ routers/Address.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {addAddress} = require('../Controllers/Address/address');
+const {addAddress,getAddress} = require('../Controllers/Address/address');
 
 router.post('/add-address',addAddress);
+router.get('/get-address',getAddress);
 module.exports = router;
